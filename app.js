e9bac8a85c09bbdbfd33e3cbd9a0499eb841a5c6
aliw14 <aligm@code.edu.az>
2024-01-24 20:35:50 +0400

    Changes
    Add Some If else else if ternary operator

--- app.js
+++ app.js
@@ -9,8 +9,40 @@ const obj1 = {
 };
 const { name, surname, ...rest } = obj1;
 
+// If else with compare operations
+if (firstNum > 0 && firstNum % 2 == 0) {
+  console.log("true");
+} else {
+  console.log("false");
+}
+
+if (firstNum === secondNum || rest != secondNum) {
+  console.log("true");
+} else {
+  console.log("false");
+}
+
+if ((arr1[3] < arr1[4] && firstNum >= 5) || arr1[1] == secondNum) {
+  console.log("true");
+} else {
+  console.log("false");
+}
+
 if (sum > rest) {
   console.log("Array win");
 } else {
   console.log("Object win");
 }
+
+// If else else if  with typeof
+if (typeof obj1.name == "string") {
+  console.log(obj1.name, "type is string");
+} else if (typeof obj1.name == "number") {
+  console.log(obj1.name, "type is number");
+} else {
+  console.log(obj1.name, "type is boolean");
+}
+
+//Ternary Operator
+const userName = "Xasi";
+console.log(userName === obj1.name ? "True Name" : "False Name");
